2ab66884472ed9c2d854f2279b57d1e04a88b6aa
Marcos <marcosugaldemartinez@gmail.com>
2024-02-01 15:27:39 +0100

    feat: exported all user queries

---
+++ backend-social-network/models/index.js
@@ -0,0 +1,99 @@
+const {
+  insertUser,
+  selectUser,
+  selectAllUsers,
+  selectUserByUsername,
+} = require("./queries");
+
+const createUser = (db) => async (email, username, password, public) => {
+  try {
+    await db.query(insertUser(email, username, password, public));
+
+    return {
+      ok: true,
+    };
+  } catch (error) {
+    console.info("Create user error: ", error.message);
+    return {
+      ok: false,
+      message: error.message,
+    };
+  }
+};
+
+const getUser = (db) => async (email, compareFn) => {
+  try {
+    const user = await db.maybeOne(selectUser(email));
+
+    if (!user)
+      return {
+        ok: false,
+        error_code: "wrong_data",
+      };
+
+    const areEqual = await compareFn(user.password);
+
+    if (!areEqual)
+      return {
+        ok: false,
+        error_code: "wrong_data",
+      };
+    return {
+      ok: true,
+      content: {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+        public: user.public,
+      },
+    };
+  } catch (error) {
+    console.info("Select user error: ", error.message);
+    return {
+      ok: false,
+      error: error.message,
+    };
+  }
+};
+
+const getUserByUsername = (db) => async (username) => {
+  try {
+    const response = await db.query(selectUserByUsername(username));
+
+    return {
+      ok: true,
+      response: response.rows,
+    };
+  } catch (error) {
+    console.info("User by username error: ", error.message);
+
+    return {
+      ok: false,
+      message: error.message,
+    };
+  }
+};
+
+const getAllUsers = (db) => async (input) => {
+  try {
+    const response = await db.query(selectAllUsers(input));
+    return {
+      ok: true,
+      response: response.rows,
+    };
+  } catch (error) {
+    console.info("Select all users error: ", error.message);
+
+    return {
+      ok: false,
+      message: error.message,
+    };
+  }
+};
+
+module.exports = {
+  createUser,
+  getUser,
+  getUserByUsername,
+  getAllUsers,
+};
