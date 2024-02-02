248ba5e2c9c09139ca0873df03d3df9b5e977bb5
Marcos <marcosugaldemartinez@gmail.com>
2024-02-02 11:19:04 +0100

    feat: successfully created route for getting logged user data

--- backend-social-network/routes/index.js
+++ backend-social-network/routes/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 
 const authRoutes = require("./auth");
+const userRoutes = require("./users");
 
 module.exports = (db) => {
   router.use("/auth", authRoutes(db));
+  router.use("/user", userRoutes(db));
 
   return router;
 };
--- backend-social-network/routes/users.js
+++ backend-social-network/routes/users.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const { authorizer } = require("../middlewares");
 
+const userControllers = require("../controllers/user");
+
 module.exports = () => {
-  // router.get()
+  router.get("/", authorizer(), userControllers.getUser());
 
   return router;
 };
