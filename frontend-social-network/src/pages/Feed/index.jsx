4a4c5dd7bb9cbc7db9e2a2b7c7f13bff9276fbfb
Marcos <marcosugaldemartinez@gmail.com>
2024-02-19 12:45:56 +0100

    feat: implemented search results page

--- frontend-social-network/src/pages/Feed/index.jsx
+++ frontend-social-network/src/pages/Feed/index.jsx
@@ -1,17 +1,17 @@
 import Styled from './styles'
-import { usePublications } from "../../hooks"
+import { usePublications, useUser } from "../../hooks"
 import NavBar from '../../components/Navbar'
 
 const Feed = () => {
     const { data: response, isLoading } = usePublications()
-    console.log('Data para renderizar los posts: ', response)
+    const user = useUser().data?.username
 
     if(isLoading) return <p>Loading...</p>
 
     return (
         <>
             <NavBar />
-            <h1>hello word</h1>
+            <h1>Welcome back {user}!</h1>
             <Styled.List>
                 {response?.content?.map((publication) => {
                     return (
---
+++ frontend-social-network/src/pages/UsersSearch/index.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../../components/Navbar"
+
+const UsersSearch = () => {
+    return (
+        <>
+            <NavBar />
+            Hi there!
+        </>
+    )
+}
+
+export default UsersSearch
