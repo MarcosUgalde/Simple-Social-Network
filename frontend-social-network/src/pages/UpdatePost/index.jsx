1449bba15fee626ad55b4d4226eae08c463e489f
Marcos <marcosugaldemartinez@gmail.com>
2024-02-22 11:18:52 +0100

    style: added styles to update post page

--- frontend-social-network/src/pages/UpdatePost/index.jsx
+++ frontend-social-network/src/pages/UpdatePost/index.jsx
@@ -1,3 +1,4 @@
+import Styled from './styles'
 import { useForm } from "react-hook-form"
 import NavBar from "../../components/Navbar"
 import { useId, useOnePublication, usePublicationUpdate } from "../../hooks"
@@ -12,12 +13,14 @@ function UpdatePost () {
     return (
         <>
             <NavBar />
-            <form onSubmit={handleSubmit(doUpdate)}>
-            <input type="hidden" name="publicationId" id="publicationId" {...register('publicationId', {required: true})} value={id}  />
-                <input type="text" name="title" id="title" {...register('title', {required: true})} defaultValue={publication?.data?.content[0]?.title} />
-                <input type="text" name="post_text" id="post_text" {...register('post_text', {required: true})} defaultValue={publication?.data?.content[0]?.post_text} />
-                <input type="submit" value='Save changes' />
-            </form>
+            <Styled.Body>
+                <Styled.Form onSubmit={handleSubmit(doUpdate)}>
+                    <input type="hidden" name="publicationId" id="publicationId" {...register('publicationId', {required: true})} value={id}  />
+                    <Styled.Input type="text" name="title" id="title" {...register('title', {required: true})} defaultValue={publication?.data?.content[0]?.title} />
+                    <Styled.Text type="text" name="post_text" id="post_text" {...register('post_text', {required: true})} defaultValue={publication?.data?.content[0]?.post_text} />
+                    <Styled.Submit type="submit" value='Save changes' />
+                </Styled.Form>
+            </Styled.Body>
         </>
     )
 }
